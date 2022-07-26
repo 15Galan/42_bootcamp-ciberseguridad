73d8a6d35bc429c880f8bf91aaebddd68795415a
Galán <antoniogalanG15@gmail.com>
2022-07-27 00:06:49 +0200

    Línea de comandos implementada
    El script ahora puede recibir argumentos como si fuera un comando.

---
+++ ft_otp/ft_otp.py
@@ -0,0 +1,109 @@
+import argparse
+import re
+import os
+
+
+"""
+PROBLEMA:
+Crear un programa que permita registrar una clave inicial y genere una contraseña nueva cada vez que se solicite.
+Puede utilizarse cualquier librería que no sea TOTP (es decir, que no haga el trabajo sucio).
+"-g": recibe un fichero con una clave hexadecimal de 64 caracteres mínimo y la guarda en un fichero "ft_otp.key".
+"-k": recibe un fichero cifrado (como "ft_otp.key") y genera una contraseña temporal, mostrándola por pantalla.
+El fichero "ft_otp.key" siempre estará cifrado.
+"""
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+# Leer los argumentos de entrada de la línea de comandos.
+def leer_argumentos():
+    # Inicializar el analizador de argumentos.
+    analizador = inicializar_analizador()
+
+    return analizador.g, analizador.k
+
+
+# Inicializar el parser de la línea de comandos.
+def inicializar_analizador():
+    # Analizador de argumentos de la línea de comandos.
+    analizador = argparse.ArgumentParser(
+        # prog="./ft_otp",
+        description="Herramienta casera para generar contraseñas TOTP.",
+        epilog="Ejercicio 'ft_otp' del Bootcamp de Ciberseguridad de la Fundación 42 (Málaga).",
+    )
+
+    # Agregar las opciones del comando.
+    analizador.add_argument(
+        "-g",
+        metavar="fichero",
+        help="almacena una clave hexadecimal de 64 caracteres mínimo en un fichero 'ft_otp.key'.",
+        type=str)
+    analizador.add_argument(
+        "-k",
+        metavar="fichero",
+        help="genera una contraseña temporal usando un fichero y la muestra por pantalla.",
+        type=str)
+    # analizador.add_argument(
+    #     "-t",
+    #     metavar="segundos",
+    #     help="indica el tiempo de caducidad de las contraseñas generadas.",
+    #     default=int)
+
+    # Obtener los argumentos de la línea de comandos.
+    return analizador.parse_args()
+
+
+# Verifica que un fichero contiene una clave que cumple los requisitos mínimos.
+def validar_fichero(fichero):
+    global clave
+
+    # El fichero existe y es legible.
+    if not (os.path.isfile(fichero) or os.access(fichero, os.R_OK)):
+        print("Error: El fichero no existe o no es legible.")
+
+        return False
+
+    # Extraer clave del interior del fichero.
+    with open(fichero, "r") as f:
+        clave = f.read()
+
+    # Verifica que la clave es hexadecimal y mide al menos 64 caracteres.
+    if not re.match(r'^[0-9a-fA-F]{64,}$', clave):
+        print("La clave no es hexadecimal o tiene menos de 64 caracteres.")
+
+        """
+        Expresión regular:
+        ^           : límite inicial de la acotación de la cadena.
+        [0-9a-fA-F] : cualquier caracter hexadecimal (números, o letras desde 'a' hasta 'f' o desde 'A' hasta 'F').
+        {64,}       : cuantificador que indica longitud de 64 hasta ilimitados caracteres.
+        $           : límite final de la acotación de la cadena.
+        """
+
+        return False
+
+    return True
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    # Leer los argumentos de la línea de comandos.
+    fichero_clave_compartida, fichero_cifrado = leer_argumentos()
+
+    # Si se solicitó generar una clave (-g).
+    if fichero_clave_compartida and validar_fichero(fichero_clave_compartida):
+        # Almacenar la clave en un fichero '.key'.
+        with open("ft_otp.key", "w") as f:
+            f.write(clave)
+
+        print("Clave almacenada en 'ft_otp.key'.")
+
+        # TODO: cifrar el fichero '.key' con una clave aleatoria.
+
+    # Si se solicitó generar una contraseña (-k).
+    elif fichero_cifrado:
+        # TODO: Generar una contraseña temporal.
+        pass
+    
