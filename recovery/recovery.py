3cfbb7a2106fbd03f00e50e8c5e23848a8ef047d
Galán <antoniogalanG15@gmail.com>
2022-08-11 22:51:21 +0000

    Manejo de fechas y búsqueda de programas instalados implementados
    El programa gestiona las fechas (argumentos) adecuadamente.
    Además, se implementó la búsqueda de programas instalados.
    Se observa la estructura del resto del código, comentada.

---
+++ recovery/recovery.py
@@ -0,0 +1,261 @@
+"""
+Crear un programa que, dado un rango de fechas, sea capaz de extraer diversa información
+de un sistema Windows como la actividad del usuario, los programas abiertos, el historial
+de navegación, distinta información del registro de Windows... en dicho rango de tiempo.
+
+El programa recibirá un rango de fechas y mostrará por consola una lista ordenada de ficheros,
+directorios y programas abiertos en dicho rango de tiempo.
+
+Usará el módulo 'argparse' para recibir los parámetros de entrada.
+"""
+
+import win32process
+import win32evtlog
+import argparse
+import datetime
+import wmi
+import os
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+def leer_argumentos():
+    """
+    Lee los argumentos de entrada de la línea de comandos.
+
+    :return: Valor y opciones de los argumentos leídos.
+    """
+
+    # Leer los argumentos de entrada.
+    analizador = inicializar_analizador()
+
+    # Devolver los argumentos de entrada.
+    return analizador.i, analizador.f
+
+
+def inicializar_analizador():
+    """
+    Inicializa el analizador de argumentos.
+
+    :return: Objeto del analizador de argumentos.
+    """
+
+    # Crear un analizador de argumentos.
+    analizador = argparse.ArgumentParser(
+        description='Programa para obtener información de un sistema Windows.',
+        epilog="Ejercicio 'recovery' del Bootcamp de Ciberseguridad de la Fundación 42 (Málaga)."
+    )
+
+    # Agregar las opciones al comando.
+
+    # Fecha de inicio: si no se especifica, se toma la fecha timestamp 0.
+    analizador.add_argument(
+        '-i',
+        metavar='inicio',
+        help="Fecha inicial del rango de fechas en formato 'DD-MM-AAAA' (por defecto, '01-01-1970')."
+    )
+
+    # Fecha de fin: si no se especifica, se toma la fecha timestamp de ahora.
+    analizador.add_argument(
+        '-f',
+        metavar='final',
+        help="Fecha final del rango de fechas en formato 'DD-MM-AAAA' (por defecto, hoy)."
+    )
+
+    # Devolver el analizador de argumentos.
+    return analizador.parse_args()
+
+
+def tratar_fechas(inicio, final):
+    """
+    Corrige las fechas introducidas verificando que están
+    correctamente ordenadas o que el formato es correcto.
+
+    :param inicio: Fecha de inicio del rango de fechas.
+    :param final: Fecha de fin del rango de fechas.
+
+    :return: Fechas parseadas para la ejecución del programa.
+    """
+
+    try:
+        # Comprobar los argumentos recibidos.
+        if inicio and not final:
+            inicio = datetime.datetime.strptime(inicio, '%d-%m-%Y')
+            final = datetime.datetime.now()
+
+        elif not inicio and final:
+            inicio = datetime.datetime.fromtimestamp(0)
+            final = datetime.datetime.strptime(final, '%d-%m-%Y')
+
+        elif not inicio and not final:
+            # Fecha de ejecución del programa.
+            ahora = datetime.datetime.now()
+
+            # Un mes antes de la fecha de hoy.
+            inicio = ahora - datetime.timedelta(days=30)    # Valor por defecto
+            final = ahora
+
+            print('No se indicaron fechas. Se usará ranto por defecto: 30 días atrás.\n')
+
+        else:
+            inicio = datetime.datetime.strptime(inicio, '%d-%m-%Y')
+            final = datetime.datetime.strptime(final, '%d-%m-%Y')
+
+            # Comprobar que las fechas están ordenadas.
+            if final < inicio:
+                print("La fecha inicial es posterior a la fecha final, ¿invertir fechas y ejecutar? [s/n]")
+
+                # Leer la respuesta del usuario.
+                respuesta = input().lower()
+
+                # Preguntar si se quiere invertir las fechas.
+                if respuesta == 's':
+                    inicio, final = final, inicio
+
+                    print("Fechas invertidas.")
+
+                elif respuesta == 'n':
+                    print("Has intentado desafiar las leyes del espacio-tiempo.")
+                    exit()
+
+                else:
+                    print("Opción no válida.")
+                    exit()
+
+    except:
+        print("Formato de fecha no válido.")
+        exit()
+
+    return inicio, final
+
+
+def actividad_usuario(inicio, final):
+    """
+    Obtiene la actividad del usuario en un rango de fechas.
+
+    :param inicio: Fecha de inicio del rango de fechas.
+    :param final: Fecha de fin del rango de fechas.
+
+    :return: Lista de actividad del usuario.
+    """
+
+    # TODO: implementar
+
+    return None
+
+
+def programas_abiertos(inicio, final):
+    """
+    Obtiene los programas abiertos en un rango de fechas.
+
+    :param inicio: Fecha de inicio del rango de fechas.
+    :param final: Fecha de fin del rango de fechas.
+
+    :return: Lista de programas abiertos.
+    """
+
+    # TODO: implementar
+
+    return None
+
+
+def programas_instalados(inicio, final):
+    """
+    Obtiene los programas instalados en un rango de fechas.
+
+    :param inicio: Fecha de inicio del rango de fechas.
+    :param final: Fecha de fin del rango de fechas.
+
+    :return: Lista de programas instalados.
+    """
+
+    # Lista de programas instalados.
+    programas = []
+
+    # Listar los programas instalados en la fecha 'inicio' o posterior.
+    for programa in conexion.Win32_Product():
+        # Interpretar la fecha como 'DD-MM-AAAA'.
+        fecha = datetime.datetime.strptime(programa.InstallDate, '%Y%m%d').date()
+
+        # Comprobar que la fecha está en el rango de fechas.
+        if inicio.date() <= fecha <= final.date():
+            programas.append(programa.Name)
+
+            # print("{0} :\t{1}\t{2}\t{3}".format(fecha, programa.Caption, programa.Version, programa.Vendor))
+
+    return programas
+
+
+def historial_navegacion(inicio, final):
+    """
+    Obtiene el historial de navegación en un rango de fechas.
+
+    :param inicio: Fecha de inicio del rango de fechas.
+    :param final: Fecha de fin del rango de fechas.
+
+    :return: Lista de historial de navegación.
+    """
+
+    # TODO: implementar
+
+    return None
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    # Leer los argumentos de la línea de comandos.
+    inicio, final = leer_argumentos()
+
+    # Tratar las fechas recibidas.
+    inicio, final = tratar_fechas(inicio, final)
+
+    # Establecer conexión con el sistema.
+    conexion = wmi.WMI()
+
+    # Obtener la actividad del usuario en un rango de fechas.
+    # actividades = actividad_usuario(inicio, final)
+
+    # Imprimir la actividad del usuario.
+    # print("Actividad del usuario:")
+
+    # for actividad in actividades:
+    #     print("\t" + actividad)
+
+    # Obtener programas instalados en un rango de fechas.
+    instalados = programas_instalados(inicio, final)
+
+    # Imprimir los programas instalados.
+    print("Programas instalados:")
+
+    for programa in sorted(instalados):
+        print("\t" + programa)
+
+    # Obtener los programas abiertos en un rango de fechas.
+    # abiertos = programas_abiertos(inicio, final)
+
+    # Imprimir los programas abiertos.
+    # print("Programas abiertos:")
+
+    # for programa in abiertos:
+    #     print("\t" + programa)
+
+    # Obtener el historial de navegación en un rango de fechas.
+    # historial = historial_navegacion(inicio, final)
+
+    # Imprimir el historial de navegación.
+    # print("Historial de navegación:")
+
+    # for entrada in historial:
+    #     print("\t" + entrada)
+
+    # Obtener el directorio actual.
+    # directorio_actual = os.getcwd()
+
+    # Obtener el nombre del fichero.
+    # fichero = os.path.basename(__file__)
+
+    # Obtener la ruta del fichero.
+    # ruta = os.path.join(directorio_actual, fichero)
